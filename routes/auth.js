ee828a03b8c0654e229a537d9734a8f47a802ed5
Steven Gallo <steven.gallo@outook.com>
2017-01-29 17:11:25 -0500

    add auth routes

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
+
+//when routed to /register run check to either 
+//render register page if they are not logged in
+//or send the to user page if they are
+router.get('/register', authHelpers.loginRedirect, (req, res) => {
+    res.render('auth/register');
+});
+
+//post details from new user form
+router.post('/register', (req, res, next) => {
+    return authHelpers.createUser(req, res)
+        .then((response) => {
+            console.log('registration successful');
+        })
+        .catch((err) => { res.status(500).json({ status: 'error' }); });
+});
+
+//login check and redirect
+router.get('/login', authHelpers.loginRedirect, (req, res) => {
+    res.render('auth/login');
+});
+
+//submit login info and redirect after successful login
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+}));
