5adc2c417f689381c0d6e95c3ca908b081ce87b6
Steven Gallo <steven.gallo@outook.com>
2017-01-29 16:14:43 -0500

    add passport config, local strategy, auth helpers

---
+++ auth/local.js
@@ -0,0 +1,42 @@
+//details of local strategy for logging in user with username and password
+const passport = require('passoprt');
+const LocalStrategy = require('passport-local').Strategy;
+
+const init = require('./passport');
+const models = require('../db/models/index');
+const authHelpers = require('../auth/auth-helpers');
+
+const options = {};
+
+init();
+
+//create new instance of LocalStrategy Class from passport
+passport.use(new LocalStrategy(options, (username, password, done) => {
+    // check to see if the username exists
+    models.User.findAll({
+            where: {
+                username
+            }
+        })
+        //if user exists take user from returned user array. 
+        .then((user) => {
+            //if undefined exit from checks by returning a call of done method and passing false
+            if (user[0] === undefined) {
+                return done(null, false);
+            }
+            //if comparePass called from auth-helpers returns false -passwords don't match-
+            //exit from checks by returning a call of done method and passing false
+            if (!authHelpers.comparePass(password, user[0].dataValues.password)) {
+                return done(null, false);
+                //user is not undefined and password matches so
+                //exit from checks by returning a call of done method and passing data from the user being checked
+            } else {
+                return done(null, user[0].datavalues);
+            }
+        })
+        .catch((err) => {
+            return done(err);
+        });
+}));
+
+module.exports = passport;
